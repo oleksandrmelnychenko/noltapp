3679e40e5de3c241a280e1ba35b9d0442a032cfc
artemyarotskyi <artemyarotskyi@gmail.com>
2017-05-25 17:55:27 +0300

    Remove mask from telephone field

--- Views/Colleagues/AddCollegueView.h
+++ Views/Colleagues/AddCollegueView.h
@@ -64,7 +64,7 @@ private:
 
     QString mRegName = ("[a-zA-Z0-9_\\-\\.]{1,30}");
     QString mRegEmail = ("([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_.-])+\\.([a-zA-Z]{2,4}|[0-9]{1,3})");
-    QString mRegPhone = ("\\([0-9]{3}\\)\\s[0-9]{3}-[0-9]{2}-[0-9]{2}");
+    QString mRegPhone = ("[0-9]{10}"); //("\\([0-9]{3}\\)\\s[0-9]{3}-[0-9]{2}-[0-9]{2}");
     QString mRegBirthday = ("[0-3]?[0-9].[0-3]?[0-9].([0-9]{2})?[0-9]{2}");
 
     QString mValidateColor = ("border:none; border-bottom:1px solid #909090; color:#ffffff; padding-left:7px;");
