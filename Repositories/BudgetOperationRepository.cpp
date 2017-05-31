fc4fc529b5db46e46238359f09f3b87dc19ed26a
artemyarotskyi <artemyarotskyi@gmail.com>
2017-05-31 12:58:13 +0300

    Add budgetOperationRepository

--- Repositories/BudgetOperationRepository.cpp
+++ Repositories/BudgetOperationRepository.cpp
@@ -1,6 +1,51 @@
 #include "BudgetOperationRepository.h"
 
-BudgetOperationRepository::BudgetOperationRepository(QObject *parent) : QObject(parent)
+BudgetOperationRepository::BudgetOperationRepository(QNetworkAccessManager *networkManager, QObject *parent)
+    : mNetworkManager(networkManager),
+      QObject(parent)
 {
 
+}
+
+QJsonObject *BudgetOperationRepository::GetBudgetHistory()
+{
+    QUrl url(mGetBudgetHistory);
+    QNetworkRequest request;
+    request.setUrl(url);
+
+    QNetworkReply *reply = mNetworkManager->get(request);
+    replyFinished(reply);
+
+    return &mResult;
+}
+
+QJsonObject *BudgetOperationRepository::CreateNewBudget(QJsonObject payment)
+{
+    QUrl url(mCreateNewBudget);
+    QNetworkRequest request;
+    request.setUrl(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json; charset=utf-8");
+
+    QJsonDocument doc;
+    doc.setObject(person);
+
+    QByteArray request_body = doc.toJson();
+
+    QNetworkReply *reply = mNetworkManager->post(request,request_body);
+    replyFinished(reply);
+
+    return &mResult;
+}
+
+void BudgetOperationRepository::replyFinished(QNetworkReply *reply)
+{
+    QEventLoop loop;
+    connect(reply, SIGNAL(finished()), &loop, SLOT(quit()));
+    loop.exec();
+
+    if(!reply->error())
+    {
+        QJsonDocument document = QJsonDocument::fromJson(reply->readAll());
+        mResult = document.object();
+    }
 }
--- Repositories/BudgetOperationRepository.h
+++ Repositories/BudgetOperationRepository.h
@@ -2,16 +2,36 @@
 #define BUDGETOPERATIONREPOSITORY_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QUrl>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QNetworkReply>
+#include <QEventLoop>
 
 class BudgetOperationRepository : public QObject
 {
     Q_OBJECT
 public:
-    explicit BudgetOperationRepository(QObject *parent = 0);
+    explicit BudgetOperationRepository(QNetworkAccessManager *networkManager, QObject *parent = 0);
 
-signals:
+public:
+    QJsonObject* GetBudgetHistory();
+    QJsonObject* CreateNewBudget(QJsonObject payment);
 
-public slots:
+protected:
+    void replyFinished(QNetworkReply* reply);
+
+    QJsonObject mResult;
+
+private:
+    QNetworkAccessManager *mNetworkManager;
+
+    const QString mGetBudgetHistory = "";
+    const QString mCreateNewBudget = "";
 };
 
 #endif // BUDGETOPERATIONREPOSITORY_H
